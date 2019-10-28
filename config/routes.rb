07a42fbaea43e5eac6d06d4fe4602695dc261331
Raff <pollo11@gmx.net>
2019-10-28 06:24:20 +0100

    added Homepage 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'news' => 'pages#news'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
