f2108ccacd6399531749b1a08240386b7dcf67a0
Raff <pollo11@gmx.net>
2019-11-20 06:24:33 +0100

    added layout changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 require jquery
 require bootstrap sprockets
+require jquery_ujs
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'news' => 'pages#news'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get '/users/sign_out' => 'devise/sessions#destroy'
+  devise_scope :user do  
+  	get '/users/sign_out' => 'devise/sessions#destroy'    
+  end 
 end
